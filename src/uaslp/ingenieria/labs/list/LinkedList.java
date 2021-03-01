a074c72d4f11c0d09ab67dbd0c6a04c770fb354c
CristoSparda <cristo.n6-@hotmail.com>
2021-02-28 23:55:13 -0600

    Corrección del Arreglo del tipo de nodo para la lista, se agrego el metodo para insertar y para eliminar un dato en la lista

--- src/uaslp/ingenieria/labs/list/LinkedList.java
+++ src/uaslp/ingenieria/labs/list/LinkedList.java
@@ -1,13 +1,110 @@
 package uaslp.ingenieria.labs.list;
-
+//Lista doblemente ligada
 public class LinkedList {
-    LinkedList head;
-    LinkedList tail;
+    private Node head;
+    private Node tail;
+    private int size;
 
-    /*intente una prueba de como manejar el nodo desde aqui, pero no me da acceso
-    Node nuevo1 = new Node();
-    nuevo1.setDato(4);
+    /**
+     * Insert data at the end of the list
+     *
+     * @param dato Data to be inserted
+     */
 
-    System.out.println(" Dato: "+nuevo1.getDato() );*/
+    public void Inserta(int dato){
+        Node nodo = new Node(dato);
 
+        nodo.setPrevious(tail);
+
+        //Si la cola tiene un dato, apunta al nuevo nodo
+        if( tail != null){
+            tail.setNext(nodo);
+        }
+
+        //Si la cabecera esta vacia, que apunte al nuevo noto / lista vacia
+        if( head == null ){
+            head = nodo;
+        }
+
+        tail = nodo;
+        size++;
+
+    }
+
+    /**
+     * @param index 0-index
+     * @return data in index
+     */
+    public int get(int index){
+        Node currentNode = head;
+        int currentIndex =0;
+
+        while(currentIndex < index){
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+
+        return currentNode.getData();
+    }
+
+    public void delete(int index) {
+        Node currentNode = head;
+        int currentIndex = 0;
+
+        //Si no lo encuentra
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        size--;
+
+        //Si no hay nada en la lista
+        if (size == 0) {
+            head = null;
+            tail = null;
+            return;
+        }
+
+        //si es el primer dato de la lista
+        if(index == 0){
+            head = head.getNext();
+            head.setPrevious(null);
+        }
+
+        //Es el ultimo dato de la lista
+        if (index == size) {
+            tail = tail.getPrevious();
+            tail.setNext(null);
+        }
+
+        //recorre la lista dentro de los limites de esta
+        if (index > 0 && index < size) {
+            while (currentIndex < index) {
+                currentNode = currentNode.getNext();
+                currentIndex++;
+            }
+
+            //esta parte aun no me queda clara...
+            currentNode.getPrevious().setNext(currentNode.getNext());
+            currentNode.getNext().setPrevious(currentNode.getPrevious());
+        }
+
+        //Iterador Patron de diseño
+
+        /*public int getSize(){
+            return size;
+        }*/
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/uaslp/ingenieria/labs/list/Node.java
+++ src/uaslp/ingenieria/labs/list/Node.java
@@ -1,32 +1,50 @@
 package uaslp.ingenieria.labs.list;
 
 class Node {
-    int dato;
-    LinkedList next;
-    LinkedList previous;
+    private int dato;
+    private Node next;
+    private Node previous;
 
+    /*
+    Codigo mio
     void setDato(int dato){
+        this.dato = dato;
+    }*/
+
+    //como debe de ser el Set para dato, regresando un tipo "Node" es deicr etructura
+    Node(int dato) {
         this.dato = dato;
     }
 
-    void setNext(LinkedList next){
-        this.next = next;
+    //y tambien uno al parecer tambien un metodo para que regrese un int
+    public void setData(int dato) {
+        this.dato = dato;
     }
 
-    void setPrevious(LinkedList previous){
-        this.previous = previous;
-    }
 
-    int getDato() {
+    public int getData(){
         return dato;
     }
 
-    LinkedList getNext() {
+
+    public void setNext(Node next){
+        this.next = next;
+    }
+
+
+    public Node getNext(){
         return next;
     }
 
-    LinkedList getPrevious() {
+
+    public Node getPrevious(){
         return previous;
     }
+
+    public void setPrevious(Node previous){
+        this.previous = previous;
+    }
+
+
 
 }
